dcbd46b5521ff5877e83b8fa5e854725b0e93d50
MariadelMar8 <91215758+MariadelMar8@users.noreply.github.com>
2022-01-25 18:17:24 +0000

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -8,7 +8,9 @@
 #include "Usuario.h"
 #include "Maquina.h"
 
-
+list<Reserva> valorReserva();
+list<Maquina> valorMaquina();
+list<Usuario> valorUsuario();
 Usuario getUsuario(list<Usuario> u,string x,int &f);
 bool exicluster(list<Maquina> m,string a);
 bool exitmaquina(list<Maquina> m,string a);
