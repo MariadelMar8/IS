74935dbeb85cf366b021ae12e7661b899739a12b
MariadelMar8 <91215758+MariadelMar8@users.noreply.github.com>
2022-01-25 18:15:29 +0000

    Update Funciones.cpp

--- Funciones.cpp
+++ Funciones.cpp
@@ -4,20 +4,39 @@
 
 #include "Funciones.h"
 
-Usuario getUsuario(list<Usuario> u,string x,int &f){
-	Usuario A;
-	for(list<Usuario>::iterator k=u.begin();k!=u.end();k++){
-		if(x==k->getCorreo()){
-			A.setCorreo(x);
-			A.setltiempo(k->getlTiempo());
-			A.setlCPU(k->getlCPU());
-			A.setlReserva(k->getlReserva());
-			A.setnReserva(k->getnReserva());
-			f++;
-		}
-	}
-	return A;
+list<Reserva> valorReserva(){
+	list<Reserva> r1;
+	Reserva rr;
+	rr.setrCorreo("i@uco.es");
+	rr.setrCPU(4);
+	rr.setDini(2);
+	rr.setDfin(5);
+	r1.push_back(rr);
+	return r1;
 }
+list<Maquina> valorMaquina(){
+	list<Maquina> m1;
+	Maquina mm;
+	mm.setMaquina("1");
+	mm.setCPU(10);
+	mm.setCluster("1");
+	list<Reserva> l=valorReserva();
+	mm.setReservas(l);
+	m1.push_back(mm);
+	return m1;
+}
+list<Usuario> valorUsuario(){
+	list<Usuario> u1;
+	Usuario uu;
+	uu.setCorreo("i@uco.es");
+	uu.setltiempo(5);
+	uu.setlCPU(6);
+	uu.setlReserva(4);
+	uu.setnReserva(1);
+	u1.push_back(uu);
+	return u1;
+}
+
 bool exicluster(list<Maquina> m,string a){
 	for(list<Maquina>::iterator j=m.begin();j!=m.end();j++){ //Comprobamos si existe el cluster
 		if(j->getCluster()==a){//existe cluster
@@ -28,7 +47,7 @@ bool exicluster(list<Maquina> m,string a){
 }
 bool exitmaquina(list<Maquina> m,string a){
 	for(list<Maquina>::iterator s=m.begin();s!=m.end();s++){
-		if(s.getMaquina()==a){ return true; }
+		if(s->getMaquina()==a){ return true; }
 	}
 	return false;
 }
