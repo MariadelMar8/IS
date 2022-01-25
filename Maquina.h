1b3d5c1acbf39d89c70f1b8d22042354bfe383bb
MariadelMar8 <91215758+MariadelMar8@users.noreply.github.com>
2022-01-25 18:19:54 +0000

    Update Maquina.h

--- Maquina.h
+++ Maquina.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <list>
 #include "Reserva.h"
+#include "Usuario.h"
 
 using namespace std;
 
@@ -24,8 +25,11 @@ public:
 	inline int getCPU(){ return CPU; }
 	inline string getCluster(){ return cluster; }
 	inline list<Reserva> getReservas(){ return r; }
-	inline void setReservas(list<Reserva> n){ r=n; }
-	int getnCPU();
+	inline void setMaquina(string nl){ maquina=nl; }
+	inline void setCPU(int nl){ CPU=nl; }
+	inline void setCluster(string nl){ cluster=nl; }
+	inline void setReservas(list<Reserva> nl){ r=nl; }
+	int getnCPUus(int nl);
 	bool disponibilidadmaquina(int ini,int fin,int c);
 };
 
